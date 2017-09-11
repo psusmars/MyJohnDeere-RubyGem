fdeafbb1a4585d66386d7c5d1c32fd25508d5179
Paul Susmarski <Paul.susmarski@gmail.com>
2017-09-11 08:52:41 -0600

    Add api fixtures

---
+++ test/api_fixtures.rb
@@ -0,0 +1,30 @@
+class APIFixtures
+  def initialize
+    @fixtures = ::JSON.parse(File.read("#{PROJECT_ROOT}/spec/fixtures.json"),
+      symbolize_names: true)
+    freeze_recursively(@fixtures)
+  end
+
+  def [](name)
+    @fixtures[name]
+  end
+
+  def fetch(*args)
+    @fixtures.fetch(*args)
+  end
+
+  def generate_json(name)
+    JSON.generate(self.fetch(name))
+  end
+
+  private
+
+  def freeze_recursively(data)
+    data.each do |k, v|
+      if v.is_a?(Hash)
+        freeze_recursively(v)
+      end
+    end
+    data.freeze
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,14 +4,14 @@ require 'byebug'
 require 'webmock/minitest'
 
 PROJECT_ROOT = File.expand_path("../../", __FILE__)
-#require File.expand_path('../api_fixtures', __FILE__)
+require File.expand_path('../api_fixtures', __FILE__)
 
 class Minitest::Test
   # Fixtures are available in tests using something like:
   #
   #   API_FIXTURES[:fields][:id]
   #
-  # API_FIXTURES = APIFixtures.new
+  API_FIXTURES = APIFixtures.new
 
   def setup
     MyJohnDeere.configure do |config|
