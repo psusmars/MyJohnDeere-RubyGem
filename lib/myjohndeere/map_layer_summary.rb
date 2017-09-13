ed08dbe5fc414bfb92fab72e9c32ac3ea06d8377
Paul Susmarski <Paul.susmarski@gmail.com>
2017-09-13 16:16:49 -0600

    Add the ability to create map layer summaries, cleaned up the test a bit

--- lib/myjohndeere/map_layer_summary.rb
+++ lib/myjohndeere/map_layer_summary.rb
@@ -9,36 +9,38 @@ module MyJohnDeere
       super(json_object, access_token)
     end
 
-    # def self.create(access_token, organization_id, field_id, 
-    #     title, caption, metadata = [], date_created=nil)
-    #   response = access_token.execute_request(:post, 
-    #     "/organizations/#{organization_id}/fields/#{field_id}/#{self.resource_base_path}/#{id}",
-    #     body: {
-    #       title: title,
-    #       text: caption,
-    #       links: [
-    #           {
-    #              rel: "owningOrganization",
-    #              uri: "#{MyJohnDeere.configuration.endpoint}/organizations/#{organization_id}"
-    #           },
-    #           {
-    #              rel: "contributionDefinition",
-    #              uri: "#{MyJohnDeere.configuration.endpoint}/#{MyJohnDeere.configuration.contribution_definition_id}"
-    #           }
-    #        ],
-    #       metadata: metadata,
-    #       dateCreated: (date_created || Time.now).strftime("%Y-%m-%dT%H:%M:%S.%LZ")
-    #     }
-    #   )
-    #   # "location"=>["https://sandboxapi.deere.com/platform/mapLayerSummaries/b161e522-c9ee-4170-b479-c34de6a40627"
-    #   created_id = response.http_headers["location"]
-    #   if !created_id.nil? then
-    #     created_id = /#{resource_base_path}\/([^\/]+)\Z/.match(created_id)[1]
-    #   else
-    #     # didn't succeed
-    #     MyJohnDeere.logger.info("Failed to create MapLayerSummary: #{response}")
-    #   end
-    #   return created_id
-    # end
+    def self.create(access_token, organization_id, field_id, 
+        title, caption, metadata = [], date_created=nil)
+      response = access_token.execute_request(:post, 
+        build_resouce_base_path!(self.list_resource_path, {field_id: field_id, organization_id: organization_id}),
+        body: {
+          title: title,
+          text: caption,
+          links: [
+              {
+                 rel: "owningOrganization",
+                 uri: "#{MyJohnDeere.configuration.endpoint}/organizations/#{organization_id}"
+              },
+              {
+                 rel: "contributionDefinition",
+                 uri: "#{MyJohnDeere.configuration.endpoint}/#{MyJohnDeere.configuration.contribution_definition_id}"
+              }
+           ],
+          metadata: metadata,
+          dateCreated: (date_created || Time.now).strftime("%Y-%m-%dT%H:%M:%S.%LZ")
+        }
+      )
+      # 201
+      #{"Content-Type"=>"text/plain", "X-Deere-Handling-Server"=>"ldxtc4", "X-Frame-Options"=>"SAMEORIGIN", "Location"=>"https://sandboxapi.deere.com/platform/mapLayerSummaries/c5e9317e-eda6-48d3-acc8-c3bca3424858", "X-Deere-Elapsed-Ms"=>"362", "Vary"=>"Accept-Encoding", "Expires"=>"Wed, 13 Sep 2017 22:00:45 GMT", "Cache-Control"=>"max-age=0, no-cache", "Pragma"=>"no-cache", "Date"=>"Wed, 13 Sep 2017 22:00:45 GMT", "Transfer-Encoding"=>"chunked", "Connection"=>"close, Transfer-Encoding"}
+      # The lowercase location shouldn't be needed but sometimes it is returned as lowercase...
+      created_id = response.http_headers["Location"] || response.http_headers["location"]
+      if !created_id.nil? then
+        created_id = /#{self.base_jd_resource}\/([^\/]+)\Z/.match(created_id)[1]
+      else
+        # didn't succeed
+        MyJohnDeere.logger.info("Failed to create MapLayerSummary: #{response}")
+      end
+      return created_id
+    end
   end
 end
--- test/test_map_layer_summary.rb
+++ test/test_map_layer_summary.rb
@@ -3,14 +3,17 @@ require File.expand_path('../test_helper', __FILE__)
 class TestMapLayerSummary < Minitest::Test
   FIXTURE = API_FIXTURES.fetch("map_layer_summary")
   FIXTURE_FOR_LIST = API_FIXTURES.fetch("map_layer_summaries")
+  ORGANIZATION_ID = "1234"
+  MLS_ID = "2516aa2c-1c0d-4dae-ba63-5c44ff172a01"
+  FIELD_ID = "2516aa2c-2c0d-4dae-ba63-5c44ff172a01"
 
   def test_retrieve()
     stub_request(:get, /\/mapLayerSummaries\/#{FIXTURE["id"]}/).
       to_return(status: 200, body: FIXTURE.to_json)
 
     mls = MyJohnDeere::MapLayerSummary.retrieve(default_access_token, FIXTURE["id"])
-    assert_equal "2516aa2c-2c0d-4dae-ba63-5c44ff172a01", mls.id
-    assert_equal "1234", mls.organization_id
+    assert_equal MLS_ID, mls.id
+    assert_equal ORGANIZATION_ID, mls.organization_id
     assert_equal "some title", mls.title
     assert_equal "description of the map layers", mls.text
     assert_equal [{"name"=>"The Name", "value"=>"The Value"}], mls.metadata
@@ -19,15 +22,30 @@ class TestMapLayerSummary < Minitest::Test
   end
 
   def test_list
-    organization_id = "1234"
-    field_id = "2516aa2c-2c0d-4dae-ba63-5c64fd172d01"
-    stub_request(:get, /organizations\/#{organization_id}\/fields\/#{field_id}\/mapLayerSummaries/).
+    stub_request(:get, /organizations\/#{ORGANIZATION_ID}\/fields\/#{FIELD_ID}\/mapLayerSummaries/).
       to_return(status: 200, body: FIXTURE_FOR_LIST.to_json)
 
     mlss = MyJohnDeere::MapLayerSummary.list(default_access_token, count: 1, 
-      organization_id: organization_id, field_id: field_id)
+      organization_id: ORGANIZATION_ID, field_id: FIELD_ID)
 
     assert_equal 1, mlss.data.length
     assert_equal MyJohnDeere::MapLayerSummary, mlss.data[0].class
+  end
+
+  def test_create
+    expected_body =  "{\"title\":\"Test number 2\",\"text\":\"Hello from farm lens again\",\"links\":[{\"rel\":\"owningOrganization\",\"uri\":\"https://sandboxapi.deere.com/platform/organizations/1234\"},{\"rel\":\"contributionDefinition\",\"uri\":\"https://sandboxapi.deere.com/platform/foobar\"}],\"metadata\":[{\"key\":\"val\"}],\"dateCreated\":\"2016-01-02T16:14:23.421Z\"}"
+    stub_request(:post, /\/organizations\/#{ORGANIZATION_ID}\/fields\/#{FIELD_ID}\/mapLayerSummaries/).
+      with(body: expected_body,
+       headers: {'Accept'=>'application/vnd.deere.axiom.v3+json', 'Content-Length'=>'319', 'Content-Type'=>'application/vnd.deere.axiom.v3+json'}).
+      to_return(status: 201, headers: {"Location"=>"https://sandboxapi.deere.com/platform/mapLayerSummaries/#{MLS_ID}"})
+    response = MyJohnDeere::MapLayerSummary.create(default_access_token, 
+      ORGANIZATION_ID,
+      FIELD_ID,
+      "Test number 2",
+      "Hello from farm lens again",
+      ["key" => "val"],
+      Time.parse("2016-01-02T16:14:23.421Z")
+    )
+    assert_equal MLS_ID, response
   end
 end
